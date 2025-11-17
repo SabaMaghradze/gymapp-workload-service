eece6275cd366fe5ee35af72bbbe65d3d20a11f7
Saba Maghradze <saba@Sabas-MacBook-Pro.local>
2025-11-17 13:45:35 +0400

    fixed a typo

--- src/main/java/com/gymapp/workload_service/GymappWorkloadServiceApplication.java
+++ src/main/java/com/gymapp/workload_service/GymappWorkloadServiceApplication.java
@@ -2,8 +2,10 @@ package com.gymapp.workload_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
+@EnableJms
 public class GymappWorkloadServiceApplication {
 
 	public static void main(String[] args) {
